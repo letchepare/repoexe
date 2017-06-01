c24aca331141285d863f1cf9ce9e6d785aae3541
Lilian Etchepare <contact@lilian-etchepare.fr>
2017-06-01 23:36:28 +0200

    Publication du site
    tout le site est disponible maintenant

---
+++ www/jscust/liens.js
@@ -0,0 +1,32 @@
+function boulangerie() {
+    window.location.href="https://goo.gl/pu3CDP";
+}
+function boulangerieindex()	{
+	
+	window.location.href="http://repoexesjp.livehost.fr/#boulangerie";
+}
+
+function tonneau() {
+    window.location.href="https://goo.gl/Xp80vL";
+}
+function tonneauindex()	{
+	
+	window.location.href="http://repoexesjp.livehost.fr/#tonneau";
+}
+
+
+function calcons() {
+	window.location.href="https://goo.gl/zAwlZX";
+}
+
+function calconsindex()	{
+	
+	window.location.href="http://repoexesjp.livehost.fr/#calcons";
+}
+
+
+
+function repoexe()			{
+	
+	window.location.href="http://repoexesjp.livehost.fr";
+}
